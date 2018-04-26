ad334c13afe786f29cf085f2619b29003107767e
vagrant <vagrant@kleer-csd-box>
2018-04-26 20:36:44 -0300

    Incorporacion de arreglo

--- lib/noEstalles.rb
+++ lib/noEstalles.rb
@@ -1,16 +1,38 @@
 class NoEstalles
 
 	def initialize
-
+		@tablero = ["0","0","1","0"]
+		@puntajeJugador = 0
+		@vidasJugador = 1
 	end
+
 	def iniciarVidas
-			@vidasJugador = 3
+			@vidasJugador = 1
 			return @vidasJugador
 	end
 
 	def iniciarPuntos
 			@puntajeJugador = 0
 			return @puntajeJugador
+	end
+
+	def getVidas
+		return @vidasJugador
+	end
+
+	def getPuntos
+		return @puntajeJugador
+	end
+
+	def eligeCelda posicion
+
+			if @tablero[posicion] = 0
+				@puntajeJugador += 1
+			else
+					@vidasJugador = 0
+			end
+
+		return @puntajeJugador
 	end
 
 end
--- spec/noEstalles_spec.rb
+++ spec/noEstalles_spec.rb
@@ -4,7 +4,21 @@ describe "Juego No Estalles!" do
 
 	it "Inicio del juego" do
 		tablero = NoEstalles.new
-		tablero.iniciarVidas.should == 3
+		tablero.iniciarVidas.should == 1
 		tablero.iniciarPuntos.should == 0
 	end
+	
+	it "Selecciona fila 1, columna 1 y sigue vivo" do
+		tablero = NoEstalles.new
+		tablero.eligeCelda 1
+		tablero.getPuntos.should == 1
+		tablero.getVidas.should == 1
+	end
+
+	it "Selecciona fila 1, columna 2 y sigue vivo" do
+		tablero = NoEstalles.new
+		tablero.eligeCelda 2
+		tablero.getVidas.should == 1
+	end
+
 end
